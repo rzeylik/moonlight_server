3a7d2b84a454065164c9b0fe1aa92e0618009992
Vlad Barabolya <barabolya.vlad@gmail.com>
2021-09-27 13:19:54 +0300

    Redeploy

--- config/database.js
+++ config/database.js
@@ -8,7 +8,6 @@ const Helpers = use('Helpers')
 
 const Url = require('url-parse')
 const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
-
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -19,13 +18,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: {
-    host: Env.get('DB_HOST', DATABASE_URL.host),
-    port: Env.get('DB_PORT', ''),
-    user: Env.get('DB_USER', DATABASE_URL.username),
-    password: Env.get('DB_PASSWORD', DATABASE_URL.password),
-    database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
-  },
+  connection: 'pg',
 
   /*
   |--------------------------------------------------------------------------
@@ -80,11 +73,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', DATABASE_URL.host),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      user: Env.get('DB_USER', DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
     }
   }
 }
