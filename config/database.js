6fb47c2582a7088879d72f8c076e5021e4bdb0d7
Vlad Barabolya <barabolya.vlad@gmail.com>
2021-09-27 13:01:11 +0300

    Deploy

--- config/database.js
+++ config/database.js
@@ -6,6 +6,9 @@ const Env = use('Env')
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
 
+const Url = require('url-parse')
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -16,7 +19,13 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: {
+    host: Env.get('DB_HOST', DATABASE_URL.host),
+    port: Env.get('DB_PORT', ''),
+    user: Env.get('DB_USER', DATABASE_URL.username),
+    password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+    database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
+  },
 
   /*
   |--------------------------------------------------------------------------
